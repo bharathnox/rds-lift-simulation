3187ad51f1194c59ee8ee1bbaf93b93f9d45e233
Bharath Chandra <bharathnox@gmail.com>
2024-09-05 17:15:17 +0530

    from scratch

--- main.js
+++ main.js
@@ -1,80 +1,168 @@
-let objPosition = 0;
-const moveDistance = 150;
+function generate() {
 
-function moveToFloor(floorIndex) {
-    const newPosition = -floorIndex * moveDistance;
-    
-    if (newPosition <= 0 && newPosition >= -(3 * moveDistance)) {
-        objPosition = newPosition;
+    const simulation = document.getElementById("simulation");
+    const liftsValue = document.getElementById("liftsInput").value;
+    const floorsValue = document.getElementById("floorsInput").value;
 
-        const obj = document.getElementById('obj');
-        obj.style.transform = `translateY(${objPosition}px)`;
+    simulation.innerHTML = ''
+
+    simulation.style.position = 'relative'
+
+    const liftWidth = 50;
+    const spacing = 60;
+
+    for (var i = 1; i <= liftsValue; i++){
+
+        const lift = document.createElement("div");
+        lift.className = "lift"
+
+        const txt = document.createTextNode("Lift "+i);
+        lift.appendChild(txt);
+        lift.className = "lift";
+        lift.id = "lift" + i;
+
+        simulation.appendChild(lift);
+
+        lift.style.display = 'flex';
+        lift.style.flexDirection = 'row';
+        lift.style.width = liftWidth + 'px';
+        lift.style.height = '80px';
+        lift.style.backgroundColor = 'blue';
+        lift.style.position = 'absolute';
+        lift.style.bottom = '0';
+        lift.style.left = (i - 1) * (liftWidth + spacing) + 'px';
+        lift.style.marginLeft = '130px'
+
     }
-}
 
-function up(currentFloor) {
-    const newFloorIndex = currentFloor - 1;
-    moveToFloor(newFloorIndex);
-}
+    for (var i = floorsValue; i >= 1; i--){
 
-function down(currentFloor) {
-    const newFloorIndex = currentFloor - 1;
-    moveToFloor(newFloorIndex);
-}
-
-function liftsInput(){
-    var liftsinput = document.getElementById('liftsInput');
-    var liftsValue = liftsinput.value;
-    console.log("no.of lifts are ",liftsValue)
-
-    const rm = document.getElementById("lInput")
-    rm.remove();
-
-    for(var i=liftsValue; i>=1; i--){
-        const txt = document.createTextNode("Lift " + i);
-        const para = document.createElement("p");
-        para.appendChild(txt);
-        const lifts = document.getElementById("lifts");
-        lifts.appendChild(para);
-    }
-}
-
-function floorsInput(){
-
-    var floorsinput = document.getElementById('floorsInput');
-    var floorsValue = floorsinput.value;
-    console.log("no.of floors are ",floorsValue)
-
-    const rm = document.getElementById("fInput")
-    rm.remove();
-
-
-    for(var i=floorsValue; i>=1; i--){
+        const floors = document.createElement("div");
+        floors.className = "floors"
+        floors.id = "floor"+i;
+        const btns = document.createElement("div");
+        btns.className = "btns"
 
         const up = document.createElement("button");
-        up.textContent = 'UP';
+        up.className = "up"
+        up.id = i
+
+        let move = 102;
+        let upid = up.id;
+
+        up.addEventListener('click', function up() {
+
+            let liftPosi = 1;
+            
+            if (Math.abs(upid-floorsValue) < Math.abs(upid-1 )) {
+                for (var j = upid; j <= floorsValue; j++){
+                    if (liftPosi == j) {
+                        liftPosi = upid;
+                        for(var a=1; a<=liftsValue; a++){
+                            const lift = document.getElementById('lift'+a);
+                            lift.style.transition = '2s'
+                            lift.style.bottom = (liftPosi - 1) * move + 'px'
+                        }
+                        console.log("if-if")
+                    }
+                    console.log("if-for")
+                }
+            } else {
+                for (var j = upid; j >= 1; j--){
+                    if (liftPosi == j) {
+                        liftPosi = upid;
+                        for(var a=1; a<=liftsValue; a++){
+                            const lift = document.getElementById('lift'+a);
+                            lift.style.transition = '2s'
+                            lift.style.bottom = (liftPosi - 1) * move + 'px'
+                        }
+                        console.log(liftPosi)
+                    }
+                    console.log("else-for")
+                }
+            }
+
+            // for (var j = upid; j <= floorsValue; j++){
+            //     for (var a = 1; a <= liftsValue; a++) {
+            //         if (liftPosi > j) {
+            //             const lift = document.getElementById('lift'+a);
+            //             lift.style.transition = '2s'
+            //             lift.style.bottom = (upid-1) * move + 'px'
+            //             liftPosi -= j;
+            //             a = liftPosi;
+            //             console.log('if1')
+            //         }
+            //         if (liftPosi < j) {
+            //             const lift = document.getElementById('lift'+a);
+            //             lift.style.transition = '2s'
+            //             lift.style.bottom = (upid-1) * move + 'px'
+            //             liftPosi += j;
+            //             a = liftPosi;
+            //             console.log('if2')
+            //         }
+            //     }
+            // }
+            // for (var j = upid; j >= 1; j--){
+            //     for (var a = 1; a <= liftsValue; a++) {
+            //         if (liftPosi < j) {
+            //             const lift = document.getElementById('lift'+a);
+            //             lift.style.transition = '2s'
+            //             lift.style.bottom = (upid-1) * move + 'px'
+            //             liftPosi += j;
+            //             a = liftPosi;
+            //             console.log('if3')
+            //         }
+            //         if (liftPosi > 1) {
+            //             const lift = document.getElementById('lift'+a);
+            //             lift.style.transition = '2s'
+            //             lift.style.bottom = (upid-1) * move + 'px'
+            //             liftPosi -= j;
+            //             a = liftPosi;
+            //             console.log('if4')
+            //         }
+            //     }
+            // }
+            // for (var a = 1; a <= liftsValue; a++) {
+            //     const lift = document.getElementById('lift'+a);
+            //     lift.style.transition = '2s'
+            //     lift.style.bottom = (upid-1) * move + 'px'
+            //     liftPosi += move;
+            // }
+        });
         const down = document.createElement("button");
-        down.textContent = 'DOWN';
-        const hr = document.createElement("hr")
-        const txt = document.createTextNode("Floor " + i)
-        const para = document.createElement("p")
-        para.appendChild(txt);
+        down.className = "down"
 
-        const floors = document.getElementById("floors");
-        const floor = document.getElementById("floor");
-        floor.appendChild(up);
-        floor.appendChild(down);
-        floor.appendChild(hr);
-        floor.appendChild(para);
-        // floor.style.border = '2px solid red'
-        // floor.style.padding = '50px'
+        down.addEventListener('click', function down() {
+            for (var a = 1; a <= liftsValue; a++) {
+                const lift = document.getElementById('lift'+a);
+                lift.style.transition = '2s'
+                lift.style.bottom = (upid-1) * move + 'px'
+            }
+        });
 
-        floors.appendChild(floor);
-        // floors.style.marginLeft = '300px'
-        // floors.style.marginRight = '300px'
-        // floors.style.marginTop = '10%'
-        // floors.style.border = '2px solid black'
-        // floors.style.padding = '50px'
-        // floors.style.gap = '50px'
+        const para = document.createElement("p");
+        const hr = document.createElement("hr");
+        const upTxt = document.createTextNode("UP");
+        const downTxt = document.createTextNode("DOWN");
+        const floorTxt = document.createTextNode("Floor " + i);
+
+        up.appendChild(upTxt)
+        down.appendChild(downTxt)
+        para.appendChild(floorTxt)
+        btns.appendChild(up)
+        btns.appendChild(down)
+        floors.appendChild(btns)
+        floors.appendChild(para)
+        floors.appendChild(hr)
+
+        hr.style.border = "1px solid black";
+
+        floors.style.textAlign = 'right'
+
+        btns.style.display = 'flex'
+        btns.style.flexDirection = 'column'
+        btns.style.width = '100px'
+
+        simulation.appendChild(floors)
     }
 }
